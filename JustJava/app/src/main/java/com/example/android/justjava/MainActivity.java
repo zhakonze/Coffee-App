ce5b95959e3af0391274ae37d9ff65788bd0ea7f
Zakhele HAKONZE <zhakonze@e5r1p1.wethinkcode.co.za>
2018-06-23 09:34:42 +0200

    With Email support

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -11,6 +11,8 @@ package com.example.android.justjava;
 
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -83,11 +85,21 @@ public class MainActivity extends AppCompatActivity {
         CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.choclate_checker);
         boolean _chocolate = chocolateCheckBox.isChecked();
         boolean _hasWhipCream = whippedCreamCheckBox.isChecked();
-        if (price > 0)
+        if (price > 0) {
             msg = createOrderSummary(price, _hasWhipCream, _chocolate, _name);
+            Intent intent = new Intent(Intent.ACTION_SENDTO);
+            intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+            intent.putExtra(Intent.EXTRA_EMAIL, "Justjava@coffee.com");
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java order for " + _name);
+            intent.putExtra(Intent.EXTRA_TEXT, msg);
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            }
+        }
         else
             msg = createErrorMsg();
         displayMessage(msg);
+
 
 
     }
